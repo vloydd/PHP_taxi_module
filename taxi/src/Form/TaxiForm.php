3d400a981cab3de63563ec70859a499d915bbe4b
vloyd <ma7kitan@gmail.com>
2022-03-31 14:40:22 +0300

    same time fix

--- taxi/src/Form/TaxiForm.php
+++ taxi/src/Form/TaxiForm.php
@@ -26,15 +26,13 @@ class TaxiForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
 
-    $form['#prefix'] = '<div id="form-wrapper">';
+    $form['#prefix'] = '<div id="form-wrapper" class="col-md-6 col-xs-12 ml-auto mr-auto">';
     $form['#suffix'] = '</div>';
     $form['message'] = [
       '#type' => 'markup',
       '#markup' =>
       '<h3>Now You Can Book a Taxi</h3>',
     ];
-    $form['#prefix'] = '<div id="form-wrapper" class="col-md-6 col-xs-12 ml-auto mr-auto">';
-    $form['#suffix'] = '</div>';
     $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Name'),
@@ -42,38 +40,23 @@ class TaxiForm extends FormBase {
       '#required' => TRUE,
       '#wrapper_attributes' => ['class' => 'col-xs-12'],
       '#maxlength' => 100,
-      '#ajax' => [
-        'callback' => '::validateFormAjaxName',
-        'event' => 'change',
-        'progress' => [
-          'type' => 'none',
-        ],
-      ],
       '#attributes' => [
         'data-disable-refocus' => 'true',
         'autocomplete' => 'off',
       ],
-      '#suffix' => '<p class="false_form false_name"></p>',
     ];
     $form['email'] = [
-      '#required' => TRUE,
       '#type' => 'email',
       '#title' => $this->t('Email'),
       '#description' => $this->t('Only Alpha, ., _, - and @ Allowed'),
       '#placeholder' => $this->t("Enter Your Email"),
       '#maxlength' => 100,
-      '#ajax' => [
-        'callback' => '::validateFormAjaxEmail',
-        'event' => 'change',
-        'progress' => [
-          'type' => 'none',
-        ],
-      ],
+      '#required' => TRUE,
+      '#wrapper_attributes' => ['class' => 'col-xs-12'],
       '#attributes' => [
         'data-disable-refocus' => 'true',
         'autocomplete' => 'off',
       ],
-      '#suffix' => '<p class="false_form false_email"></p>',
     ];
     $form['time'] = [
       '#type' => 'datetime',
@@ -89,7 +72,8 @@ class TaxiForm extends FormBase {
       '#min' => 0,
       '#max' => 10,
       '#required' => TRUE,
-      '#description' => $this->t('Only Alpha, ., _, - and @ Allowed'),
+      '#wrapper_attributes' => ['class' => 'col-6'],
+      '#description' => $this->t('Only 0-10 Allowed'),
       '#placeholder' => $this->t("Enter Amount of Adults"),
     ];
     $form['children'] = [
@@ -99,6 +83,7 @@ class TaxiForm extends FormBase {
       '#min' => 0,
       '#max' => 10,
       '#default_value' => 0,
+      '#wrapper_attributes' => ['class' => 'col-6'],
       '#description' => $this->t('Only 0-10 Allowed'),
       '#placeholder' => $this->t("Enter Amount of Children"),
     ];
@@ -106,6 +91,7 @@ class TaxiForm extends FormBase {
       '#type' => 'select',
       '#title' => $this->t("To/From Airport"),
       '#required' => TRUE,
+      '#wrapper_attributes' => ['class' => 'col-6'],
       '#options' => [
         'to' => $this->t('To'),
         'from' => $this->t('From'),
@@ -115,6 +101,7 @@ class TaxiForm extends FormBase {
       '#type' => 'select',
       '#title' => $this->t("Your Tariff"),
       '#required' => FALSE,
+      '#wrapper_attributes' => ['class' => 'col-6'],
       '#options' => [
         'eco' => $this->t('Eco'),
         'fast' => $this->t('Fast'),
@@ -126,7 +113,7 @@ class TaxiForm extends FormBase {
       '#button_type' => 'primary',
       '#value' => $this->t('Order Now'),
       '#attributes' => [
-        'class' => ['btn', 'btn-warning'],
+        'class' => ['btn', 'btn-danger'],
       ],
       '#ajax' => [
         'callback' => '::setMessage',
@@ -146,17 +133,44 @@ class TaxiForm extends FormBase {
     $email = $form_state->getValue('email');
     $adults = $form_state->getValue('adults');
     $children = $form_state->getValue('children');
-    $requiers_name = "/[-_'A-Za-z0-9 ]/";
-    $requiers_email = '/[-_@A-Za-z.0-9]/';
+    $requires_name = "/[-_'A-Za-z0-9 ]/";
+    $requires_email = '/\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/';
     $length_name = strlen($name);
     $length_email = strlen($email);
     $time = strtotime($form_state->getValue('time'));
     $timestamp = time();
+    $queries = \Drupal::database()->select('taxi', 't');
+    $queries->fields('t', ['time']);
+    $results = $queries->execute()->fetchAll();
+    $requests = [];
+    foreach ($results as $data) {
+      $requests[] = [
+        'time' => $data->time,
+      ];
+    }
+    for ($i = 0; $i < count($requests); $i++) {
+      if ($time == $requests[$i]['time']) {
+        $form_state->setErrorByName(
+          'time',
+          $this->t(
+            "Time: Sorry, We Already Have a Request on This Time(."
+          )
+        );
+      }
+    }
     if ($adults == 0 && $children == 0) {
       $form_state->setErrorByName(
         'adults',
         $this->t(
-          "Taxi: You Cannot Book an Empty Taxi(."
+          "Taxi: You Can't Book an Empty Taxi(."
+        )
+      );
+    }
+    if ($adults == 0 && $children != 0) {
+      $form_state->setErrorByName(
+        'children',
+        $this->t(
+          "Taxi: You Can't Let a Child Go Alone(."
         )
       );
     }
@@ -193,7 +207,7 @@ class TaxiForm extends FormBase {
       );
     }
     for ($i = 0; $i < $length_name; $i++) {
-      if (!preg_match($requiers_name, $name[$i])) {
+      if (!preg_match($requires_name, $name[$i])) {
         $form_state->setErrorByName('name',
           $this->t(
             "Name: Oh No! In Your Name %title You False Symbols(. Acceptable is: A-Z, 0-9 _ and '.", ['%title' => $name]
@@ -203,7 +217,7 @@ class TaxiForm extends FormBase {
     }
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
       for ($i = 0; $i < $length_email; $i++) {
-        if (!preg_match($requiers_email, $email[$i])) {
+        if (!preg_match($requires_email, $email[$i])) {
           $form_state->setErrorByName('email',
             $this->t(
               'Mail: Oh No! Your Email %title is Invalid(', ['%title' => $email]
@@ -212,148 +226,6 @@ class TaxiForm extends FormBase {
         }
       }
     }
-  }
-
-  /**
-   * Validates Our Name with AJAX.
-   *
-   * @param array $form
-   *   Comment smth.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   Comment smth.
-   *
-   * @return \Drupal\Core\Ajax\AjaxResponse
-   *   Comment smth.
-   */
-  public function validateFormAjaxName(array &$form, FormStateInterface $form_state): AjaxResponse {
-    $name = $form_state->getValue('name');
-    $emptyname = empty($name);
-    $response = new AjaxResponse();
-    $length_name = strlen($name);
-    $requiers_name = "/[-_'A-Za-z0-9 ]/";
-    if (($length_name > 100 || $length_name < 2) && $length_name != 0) {
-      $message = $this->t('Name: Oh No! Your Name %name  Have False Length. The Length: %length.',
-        ['%name' => $name, '%length' => $length_name]);
-      $response->addCommand(
-        new HtmlCommand(
-          '.false_name',
-          $message
-        )
-      );
-      return $response;
-    }
-    for ($i = 0; $i < $length_name; $i++) {
-      if (!preg_match($requiers_name, $name[$i])) {
-        $message = $this->t("Name: Oh No! Your Name %name is Invalid(. You Should Use A-z, 0-9, and special symbols (-_').", ['%name' => $name]);
-        $response->addCommand(
-          new HtmlCommand(
-            '.false_name',
-            $message
-          )
-        );
-        return $response;
-      }
-      else {
-        $message = '';
-        $response->addCommand(
-          new HtmlCommand(
-            '.false_name',
-            $message
-          )
-        );
-        return $response;
-      }
-    }
-    if (($length_name == 0) || ($emptyname) || ($length_name <= 100 && $length_name >= 2)) {
-      $message = '';
-      $response->addCommand(
-        new HtmlCommand(
-          '.false_name',
-          $message
-        )
-      );
-      return $response;
-    }
-    $response->addCommand(new HtmlCommand('.false_name', ''));
-    return $response;
-  }
-
-  /**
-   * Validates Our Email with AJAX.
-   *
-   * @param array $form
-   *   Comment smth.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   Comment smth.
-   *
-   * @return \Drupal\Core\Ajax\AjaxResponse
-   *   Comment smth.
-   */
-  public function validateFormAjaxEmail(array &$form, FormStateInterface $form_state): AjaxResponse {
-    $email = $form_state->getValue('email');
-    $length_email = strlen($email);
-    $emptyemail = empty($email);
-    $response = new AjaxResponse();
-    $requiers = '/\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/';
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $tmp = 0;
-      for ($i = 0; $i < (strlen($email)); $i++) {
-        if (!preg_match($requiers, $email[$i])) {
-          $message = $this->t('Mail: Oh No! Your Email %title is Invalid(', ['%title' => $email]);
-          $tmp++;
-          $response->addCommand(
-            new HtmlCommand(
-              '.false_email',
-              $message
-            )
-          );
-          break;
-        }
-      }
-      if ($tmp == 0) {
-        $message = '';
-        $response->addCommand(
-          new HtmlCommand(
-            '.false_email',
-            $message
-          )
-        );
-      }
-    }
-    else {
-      $message =
-        $this->t('Mail: Oh No! Your Email %title is Invalid(', ['%title' => $email]);
-      $response->addCommand(
-        new HtmlCommand(
-          '.false_email',
-          $message
-        )
-      );
-    }
-    if ($length_email > 255) {
-      $message = $this->t(
-        'Mail: On No, Your Email is too Long. MaxLength - 255. Please, Cut it Off. Your Length: %length.',
-        ['%length' => $length_email]);
-      $response->addCommand(
-        new HtmlCommand(
-          '.false_review',
-          $message
-        )
-      );
-      return $response;
-    }
-    if (($emptyemail)) {
-      $message = '';
-      $response->addCommand(
-        new HtmlCommand(
-          '.false_email',
-          $message
-        )
-      );
-      return $response;
-    }
-    $response->addCommand(new HtmlCommand('.false_email', ''));
-    return $response;
   }
 
   /**
